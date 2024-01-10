1ae3a62da6f76fc80482f9415720806aa70a7982
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2024-01-10 10:27:43 -0500

    added javascript to modify copyright date.

--- README.md
+++
@@ -1,2 +0,0 @@
-# personal-portfolio
-A Portfolio of projects I have worked on in my Frontend development journey
---
+++ assets/scripts.js
@@ -0,0 +1,3 @@
+const copyright = document.getElementById("copyright");
+const date = new Date();
+copyright.innerHTML = `<i class="fa-solid fa-copyright"></i> Copyright  ${date.getFullYear()}  Kieni Edojah`;
