ede8bff5b2661faf66dc24701eceae75f6a9f7a0
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2024-10-13 20:00:48 -0400

    added projects
    added instagram landing page

--- assets/scripts.js
+++
@@ -1,3 +0,0 @@
-const copyright = document.getElementById("copyright");
-const date = new Date();
-copyright.innerHTML = `<i class="fa-solid fa-copyright"></i> Copyright  ${date.getFullYear()}  Kieni Edojah`;
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const copyright = document.getElementById("copyright");
+const date = new Date();
+copyright.innerHTML = `<i class="fa-solid fa-copyright"></i> Copyright  ${date.getFullYear()}  Kieni Edojah`;
+
+//==============Stick Navbar==========================
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
