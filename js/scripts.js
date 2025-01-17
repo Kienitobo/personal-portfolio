bfd7850d9aa749fdbf761a6cf0efbec95f94ddcb
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2025-01-16 19:39:53 -0500

    New Image, Contact Form, Styling
    Added an image with no background.
    Added a contact form
    Edited style sheet

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 const copyright = document.getElementById("copyright");
 const date = new Date();
-copyright.innerHTML = `<i class="fa-solid fa-copyright"></i> Copyright  ${date.getFullYear()}  Kieni Edojah`;
+copyright.innerHTML = `<i class="fa-solid fa-copyright"></i> Copyright  ${date.getFullYear()}  |   Kieni Edojah`;
 
 //==============Stick Navbar==========================
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -15,7 +17,7 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
